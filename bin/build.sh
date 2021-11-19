d16f18d28926e173312b7558781d02f63ce23136
Ximo Rovira <jrovira@iti.es>
2021-11-19 12:51:09 +0100

    Add my own ROM

--- bin/build.sh
+++ bin/build.sh
@@ -2,5 +2,5 @@
 
 mkdir -p build
 docker build -t ovmf-vbios-patch docker-build
-docker run --rm -v "$PWD/build:/build" -v "$PWD/roms:/roms" ovmf-vbios-patch /ovmf/compile-ovmf.sh rtx_3060_max-q.rom
+docker run --rm -v "$PWD/build:/build" -v "$PWD/roms:/roms" ovmf-vbios-patch /ovmf/compile-ovmf.sh Asus.RTX3080Mobile.16384.201223_1.rom
 tar -czf build/ovmf-vbios-patched.tgz build/OVMF*
